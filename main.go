f67a4ab7ca64ee1b412804fca12834786b944466
dvdmarveira <ddmdoliveira@gmail.com>
2025-05-17 20:15:41 -0300

    feat(api): create handlers to list and create tasks

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"net/http"
 
 	"github.com/dvdmarveira/simple-crud-go/config"
+	"github.com/dvdmarveira/simple-crud-go/handlers"
 	"github.com/dvdmarveira/simple-crud-go/models"
+	"github.com/gorilla/mux"
 )
 
 // Função chamada quando a aplicação é iniciada
@@ -22,6 +24,15 @@ func main() {
 				log.Fatal(err)
 		}
 
+		router := mux.NewRouter()
+
+		taskHandler := handlers.NewTaskHandler(db)
+
+		router.HandleFunc("/tasks", taskHandler.ReadTasks).Methods("GET")
+		router.HandleFunc("/tasks", taskHandler.CreateTask).Methods("POST")
+		// router.HandleFunc("/tasks/{id}", taskHandler.UpdateTask).Methods("PUT")
+		// router.HandleFunc("/tasks/{id}", taskHandler.DeleteTask).Methods("DELETE")
+
 		// Define o manipulador de rotas
-		log.Fatal(http.ListenAndServe(":8080", nil))
+		log.Fatal(http.ListenAndServe(":8080", router))
 }
