6086b13eac265ecf8e2fac10b81c3329aee27488
dvdmarveira <ddmdoliveira@gmail.com>
2025-05-17 12:59:54 -0300

    feat: create table tasks

--- main.go
+++ main.go
@@ -5,16 +5,23 @@ import (
 	"net/http"
 
 	"github.com/dvdmarveira/simple-crud-go/config"
+	"github.com/dvdmarveira/simple-crud-go/models"
 )
 
 // Função chamada quando a aplicação é iniciada
 // Ponto de entrada da aplicação
 func main() {
 		// Chama a função SetupDB
-		dbConnection := config.SetupDB()
+		db := config.SetupDB()
 
 		// Chama a função Close para fechar a conexão com o banco de dados
-		defer dbConnection.Close()
+		defer db.Close()
 
+		_, err := db.Exec(models.CreateTableSQL)
+		if err != nil {
+				log.Fatal(err)
+		}
+
+		// Define o manipulador de rotas
 		log.Fatal(http.ListenAndServe(":8080", nil))
 }
